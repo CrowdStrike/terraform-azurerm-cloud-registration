cc1d45fed78b07e16a882a39ecf267b0130351b9
Wayne Hsieh <wayne.hsieh@crowdstrike.com>
2025-10-03 11:14:36 -0400

    fix: input variables restrictions (#18)
    Issue: CSPG-69312

--- modules/log-ingestion/variables.tf
+++ modules/log-ingestion/variables.tf
@@ -78,6 +78,11 @@ variable "env" {
   description = "Environment label (for example, prod, stag, dev) used for resource naming and tagging. Helps distinguish between different deployment environments. Limited to 4 alphanumeric characters for compatibility with resource naming restrictions."
   default     = "prod"
   type        = string
+
+  validation {
+    condition     = var.env == "" || (length(var.env) <= 4 && can(regex("^[0-9a-zA-Z]*$", var.env)))
+    error_message = "The 'env' must only contain alphanumeric characters and has a limit of 4 characters."
+  }
 }
 
 variable "location" {
@@ -90,12 +95,26 @@ variable "resource_prefix" {
   description = "Prefix to be added to all created resource names for identification"
   default     = ""
   type        = string
+
+  validation {
+    condition     = length(var.resource_prefix) + length(var.resource_suffix) <= 10
+    error_message = "The combined length of resource_prefix and resource_suffix must be 10 characters or less."
+  }
+  validation {
+    condition     = var.resource_prefix == "" || can(regex("^[a-zA-Z][a-zA-Z0-9-]*$", var.resource_prefix))
+    error_message = "resource_prefix can only contain letters, numbers, and hyphens, and must start with a letter."
+  }
 }
 
 variable "resource_suffix" {
   description = "Suffix to be added to all created resource names for identification"
   default     = ""
   type        = string
+
+  validation {
+    condition     = var.resource_suffix == "" || can(regex("^[a-zA-Z0-9-]*[a-zA-Z0-9]$", var.resource_suffix))
+    error_message = "resource_suffix can only contain letters, numbers, and hyphens, and must end with a letter or number."
+  }
 }
 
 variable "tags" {
